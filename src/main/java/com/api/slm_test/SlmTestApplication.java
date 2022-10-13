c7ec81d179dcdd1657c65cb236d45ff7e7e7c8e1
dse <dse@ds-s.at>
2022-10-14 00:59:57 +0200

    initial commit

---
+++ src/main/java/com/api/slm_test/SlmTestApplication.java
@@ -0,0 +1,13 @@
+package com.api.slm_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/slm_test/SlmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.slm_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
